737197578456f07bfb2ff16a96d81930169e8784
jchoi1 <jkchoi93@gmail.com>
2014-11-23 19:44:24 -0500

    Starting to work on util/profile_test

---
+++ backend/util/profile_test.go
@@ -0,0 +1,19 @@
+package util
+
+import (
+	"testing"
+)
+
+func TestEnter(t *testing.T) {
+	profiler := &Profiler{}
+	pt := profiler.Enter("some name")
+
+	if &pt == nil {
+		t.Error("Returned a nil ProfileToken")
+	}
+}
+
+func TestExit(t *testing.T) {
+	ptoken := &ProfToken{}
+	ptoken.Exit()
+}
